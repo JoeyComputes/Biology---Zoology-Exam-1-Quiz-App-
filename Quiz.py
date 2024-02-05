fc334ae4ca002b2defea008c31febd57af1b8f3d
Joey Tosakoon <joesphgaming@gmail.com>
2024-02-05 17:31:54 -0500

    First Commit | Main App
    First Commit to the files for the Python Zoology Python App.

---
+++ Quiz.py
@@ -0,0 +1,127 @@
+
+#Quiz_Data holds most of the data for the quiz app. It is storing all the questions, choices, and answers.
+quiz_data = [  
+    {
+        "question": "From largest to smallest, what are the categories for naming an organism?",
+        "choices": [
+            "Phylum, Order, Family, Genus, Species, Kingdom, Class",
+            "Kingdom, Phylum, Class, Order, Family, Genus, Species",
+            "Species, Genus, Order, Class, Phylum, Order, Kingdom",
+            "Kingdom, Phylum, Class, Order, Family, Species, Genus"
+        ],
+        "answers": "Kingdom, Phylum, Class, Order, Family, Genus, Species"
+    },
+    {
+        "question": "Who came up with a system of taxonomic organization?",
+        "choices": [
+            "Charles Darwin", "James Watson", "Carl Linnaeus", "Gregor Mendel"
+        ],
+        "answers": "Carl Linnaeus"
+    },
+    {
+        "question": "This is the animal's way of maintaining internal conditions in a changing environment.",
+        "choices": [
+            "Conditioning", "Homeostasis", "Taxonomy", "Regulation"
+        ],
+        "answers": "Homeostasis"
+    },
+    {
+        "question": "The Scientific name of an organism consists of...",
+        "choices": [
+            "Kingdom Genus", "Genus Species", "Kingdom Phylum", "Class Species"
+        ],
+        "answers": "Genus Species"
+    },
+    {
+        "question": "The process by which two monomers are linked is called…",
+        "choices": [
+            "Dehydration Synthesis", "Hydrolysis", "Dehydration Hydrolysis", "Polymer Synthesis"
+        ],
+        "answers": "Dehydration Synthesis"
+    },
+    {
+        "question": "Proteins are a diverse group of molecules. There are twenty _____ that makes up all proteins, in different numbers and combinations.",
+        "choices": [
+            "Nucleotides", "Monomers", "Amino Acids", "Carbohydrates"
+        ],
+        "answers": "Amino Acids"
+    },
+    {
+        "question": "The pH of blood is between 7.35 and 7.45 making it...",
+        "choices": [
+            "Neutral", "Slightly Acidic", "Slightly Basic", "Very Acidic"
+        ],
+        "answers": "Neutral"
+    },
+    {
+        "question": "The monomers are nucleotides that form polymers like:",
+        "choices": [
+            "DNA (Genes)", "RNA (Protein Synthesis)", "ATP (Energy for Cellular Work)", "All of the above"
+        ],
+        "answers": "All of the above"
+    },
+    {
+        "question": "The word 'aerobic' means: ",
+        "choices": [
+            "With ATP", "With Oxygen", "Without Oxygen", "With Carbon Dioxide"
+        ],
+        "answers": "With Oxygen"
+    },
+    {
+        "question": "An enzyme is _______.",
+        "choices": [
+            "a protein", "a lipid.", "a carbohydrates.", "an amino acid."
+        ],
+        "answers": "a protein"
+    },
+    {
+        "question": "This organelle is the 'powerhouse of the cell' and is the main site for cellular respiration.",
+        "choices": [
+            "Nucleus", "Lyson", "Mitochondria", "Golgi Apparatus"
+        ],
+        "answers": "Mitochondria"
+    },
+    {
+        "question": "Only some of the energy of cellular respiration is the formation of ATP. The rest is released as _____.",
+        "choices": [
+            "ATP", "Heat", "Fuel Molecules", "Carbon Dioxide"
+        ],
+        "answers": "Heat"
+    },
+    {
+        "question": "Which of the following is NOT a part of the cytoskeleton (internal framework) of a cell?",
+        "choices": [
+            "Cilia", "Intermediate Filaments", "Mitotubules", "Microfilaments"
+        ],
+        "answers": "Cilia"
+    },
+    {
+        "question": "What organelle is responsible for the modifying and packaging of proteins?",
+        "choices": [
+            "Lysosomes", "Mitochondria", "Golgi Apparatus", "Nucleus"
+        ],
+        "answers": "Golgi Apparatus"
+    },
+    {
+        "question": "A cuttlefish can change color due to pigment changes in the ____ cells.",
+        "choices": [
+            "Algae", "Chromatophore", "Nematocyst", "Cnidocyte"
+        ],
+        "answers": "Chromatophore"
+    },
+    {
+        "question": "All eukaryotic cells have three basic parts they are which of the following?",
+        "choices": [
+            "The nucleus, cytoskeleton, and mitochondria", "The nucleus, cytoskeleton, and mitochondria", "The plasma membrane, cytoplasm, and nucleus", "The plasma membrane, cytoplasm, and nucleus"
+        ],
+        "answers": "The plasma membrane, cytoplasm, and nucleus"
+    },
+    {
+        "question": "All organisms are made of cells. Cells are the basic units of life. This statement is also referred to as: ",
+        "choices": [
+            "National Selection", "Inheritance", "Cell Theory", "The Cell Hypothesis"
+        ],
+        "answers": "Cell Theory"
+    }
+]
+
